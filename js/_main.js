69b9c59c1d69c5fe311314ba68c3af6c682d97b1
fl315ch3r <fabian.delmonte@gmail.com>
2016-11-06 11:04:26 -0300

    first commit

---
+++ js/_main.js
@@ -0,0 +1,9 @@
+(function($){
+	//Init sKrollr
+	var s = skrollr.init({
+		render: function(data){
+			//when debugging, this log the current scroll position to the console.
+			//console.log(data.curTop);
+		}
+	});
+})(jQuery);
